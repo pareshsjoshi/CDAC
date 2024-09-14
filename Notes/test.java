1ae70f622a29c1de94649fde13b01072a2a147dd
pareshsjoshi <76038539+pareshsjoshi@users.noreply.github.com>
2024-09-14 17:12:09 +0530

    Update test.java

--- Notes/test.java
+++ Notes/test.java
@@ -1,1 +1,19 @@
 
+package com.generics.usingGenerics;
+class Box<T>{
+	private T object;
+	public T getObject() {
+		return object;
+	}
+	public void setObject(T object) {
+		this.object = object;
+	}
+}
+
+public class Test {
+	public static void main(String[] args) {
+		Box<Integer> box = new Box<>();
+		box.setObject(10);
+		System.out.println(box.getObject());
+	}
+}
